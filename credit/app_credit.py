9b99db554baf4f8308a6414cc786e28d82fc1f9b
Owais Farooqui <33741377+Sharaykaka@users.noreply.github.com>
2020-12-17 04:34:46 -0800

    Add files via upload

---
+++ credit/app_credit.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  8 17:06:01 2019
+
+@author: prithvi
+"""
+import flask
+from flask import Flask, request , jsonify, render_template
+#import jinja2
+import numpy as np
+import pandas as pd
+import pickle
+
+from flask import Flask, request,jsonify,render_template
+import pickle
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+filename = 'Model_rfc.pkl'
+model = pickle.load(open(filename, 'rb'))
+
+@app.route('/')
+def man():
+    return render_template('home_credit.html')
+
+
+@app.route('/predict', methods=['POST'])
+def home():
+    data1 = request.form['a']
+    data2 = request.form['b']
+    data3 = request.form['c']
+    data4 = request.form['d']
+    data5 = request.form['e']
+    data6 = request.form['f']
+    data7 = request.form['g']
+    data8 = request.form['h']
+    data9 = request.form['i']
+    data10 = request.form['j']
+    data11 = request.form['k']
+    data12 = request.form['l']
+    data13 = request.form['m']
+    data14 = request.form['n']
+    data15 = request.form['o']
+    data16 = request.form['p']
+    data17 = request.form['q']
+    data18 = request.form['r']
+    data19 = request.form['s']
+    data20 = request.form['t']
+    data21 = request.form['u']
+    data22 = request.form['v']
+    data23 = request.form['w']
+    arr = np.array([[data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11, data12, data13, data14, data15, data16, data17, data18, data19, data20, data21, data22, data23]])
+    pred = model.predict(arr)
+    return render_template('after_credit.html',data=pred)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
